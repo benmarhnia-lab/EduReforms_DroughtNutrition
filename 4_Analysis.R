1b4e24a8d0aa72b7422339fa7a210f790b48360d
annakdim <84142836+annakdim@users.noreply.github.com>
2024-08-30 17:06:47 -0700

    Update 4_Analysis.R

--- 4_Analysis.R
+++ 4_Analysis.R
@@ -8,7 +8,6 @@
 ###############################################################
 ###############################################################
 
-
 rm(list =ls())
 
 #install.packages("lmtest")
@@ -22,7 +21,7 @@ library(sjPlot)
 library(car)
 options(scipen=999)
 
-setwd("D:/Anna/Dropbox/Projects/2024_San Diego/School reforms/Data/")
+setwd("D:/Project folder")
 
 results = data.frame(speiScale=as.character(), ageGr=as.numeric(), primReformExp=as.character() , coef=as.numeric(), SE=as.numeric(), pvalue=as.numeric(), wald.test.pval=as.character())
 
@@ -46,21 +45,14 @@ for (scale in c(2, 3, 4, 5, 6)) {
     mutate(drought_primReformExpYes = ifelse(drought==1 & primReformExp==1, 1, 0)) %>% 
     mutate(drought_primReformExpNo = ifelse(drought==1 & primReformExp==0, 1, 0))
   
-  
   ### All children under 5
   model_1 <- glm(wasted ~ drought_primReformExpYes + drought_primReformExpNo + primReformExp + as.factor(sex) + as.factor(age_child_months) + bord + as.factor(age_mother) + as.factor(birthYr_mother) + as.factor(intYr) + as.factor(intMo) + as.factor(CountryName), weight=wt, family=binomial(link='logit'), data=data)   
-  #summary(model_1)
-  #exp(coef(model_1))
-  #library(broom)
-  #tidy(model_1, exponentiate = TRUE)
 
   ## Get the robust standard errors clustered at the country level 
   model_1_coef <- coeftest(model_1, vcov. = vcovCL(model_1, cluster = data$CountryName, type = "HC0"))
   model_1_coef_df <- as.data.frame(model_1_coef[,]) %>% 
     as_tibble() %>%
     mutate(variable = rownames(model_1_coef))
- 
-  #test_1 <- linearHypothesis(model_1, c("drought_primReformExpYes=drought_primReformExpNo"))
   
   ## Extract the results
   results[nrow(results) + 1,] = c(scale, "under 5", "Yes", model_1_coef_df[2,1],  model_1_coef_df[2,2], model_1_coef_df[2,4], test_1[2,4])
@@ -77,14 +69,11 @@ for (scale in c(2, 3, 4, 5, 6)) {
   model_2_coef_df <- as.data.frame(model_2_coef[,]) %>% 
     as_tibble() %>%
     mutate(variable = rownames(model_2_coef))
-  
-  #test_2 <- linearHypothesis(model_2, c("drought_primReformExpYes=drought_primReformExpNo"))
-  
+   
   ## Extract the results
   results[nrow(results) + 1,] = c(scale, "under 3", "Yes", model_2_coef_df[2,1],  model_2_coef_df[2,2], model_2_coef_df[2,4], test_2[2,4])
   results[nrow(results) + 1,] = c(scale, "under 3", "No" , model_2_coef_df[3,1],  model_2_coef_df[3,2], model_2_coef_df[3,4], test_2[2,4])
   
-
   ### Children under 2
   data_under2 = data %>% 
     filter(age_child_months<24)
@@ -96,9 +85,7 @@ for (scale in c(2, 3, 4, 5, 6)) {
   model_3_coef_df <- as.data.frame(model_3_coef[,]) %>% 
     as_tibble() %>%
     mutate(variable = rownames(model_3_coef))
-  
-  #test_3 <- linearHypothesis(model_3, c("drought_primReformExpYes=drought_primReformExpNo"))
-  
+   
   ## Extract the results
   results[nrow(results) + 1,] = c(scale, "under 2", "Yes", model_3_coef_df[2,1],  model_3_coef_df[2,2], model_3_coef_df[2,4], test_3[2,4])
   results[nrow(results) + 1,] = c(scale, "under 2", "No" , model_3_coef_df[3,1],  model_3_coef_df[3,2], model_3_coef_df[3,4], test_3[2,4])
@@ -106,13 +93,9 @@ for (scale in c(2, 3, 4, 5, 6)) {
   
 }
 
-write.csv(results, "D:/Anna/Dropbox/Projects/2024_San Diego/School reforms/Results/results.csv")
-
-## Wald test to determine statistically significant differences in the outcomes: https://www.geeksforgeeks.org/how-to-perform-a-wald-test-in-r/#
+write.csv(results, "D:/Project folder/results.csv")
 
 ### Plot the results
-setwd("D:/Anna/Dropbox/Projects/2024_San Diego/School reforms/Results/")
-
 results <- read.csv("results.csv")[-1]
 
 results <- results %>% 
@@ -124,24 +107,21 @@ results <- results %>%
 
 unique(results$speiScale)  
 
-
 results <- results %>% 
   mutate(ageGr = ifelse(ageGr=="under 5", "Age <5 years", 
                         ifelse(ageGr=="under 3", "Age <3 years", "Age <2 years"))) %>% 
   mutate(speiScale = ifelse(speiScale=="2", "2-month drought",
                             ifelse(speiScale=="3", "3-month drought",
-                                   ifelse(speiScale=="4", "4-month drought", 
-                                          ifelse(speiScale=="5", "5-month drought", "6-month drought")))))
+                                   ifelse(speiScale=="4", "4-month drought", "5-month drought"))))
 
 
 ord <- c("Age <5 years","Age <3 years", "Age <2 years")
 results$ageGr <- factor(results$ageGr,levels=rev(ord))
 results <- results %>% mutate(ageGr = fct_reorder(ageGr, desc(ageGr))) 
 
-ord2 <- c("2-month drought", "3-month drought", "4-month drought", "5-month drought", "6-month drought")
+ord2 <- c("2-month drought", "3-month drought", "4-month drought", "5-month drought")
 results$speiScale <- factor(results$speiScale,levels=rev(ord2))
 results <- results %>% mutate(speiScale = fct_reorder(speiScale, desc(speiScale))) 
-
 
 results_2 <- results %>% 
   filter(speiScale=="2-month drought")
@@ -150,7 +130,7 @@ results_3 <- results %>%
   filter(ageGr=="Age <5 years") %>% 
   filter(speiScale %in% c("3-month drought", "4-month drought", "5-month drought"))
 
-### Prepare figures
+### Prepare the figures
 
 plot_a <-  ggplot(results_2, aes(OR, primReformExp, colour = ageGr)) +
     geom_point(size = 3.5) +
@@ -171,9 +151,7 @@ plot_a <-  ggplot(results_2, aes(OR, primReformExp, colour = ageGr)) +
     theme(strip.text = element_text(face = "italic")) +
     scale_x_continuous(limits = c(0.8, 1.55)) +
     scale_y_discrete(labels = function(x) str_wrap(x, width = 12))
-    
-    
-
+        
  plot_b <- ggplot(results_3, aes(OR, primReformExp, colour = speiScale)) +
     geom_point(size = 3.5) +
     geom_errorbar(aes(xmin = CILow, xmax = CIHigh), width = 0, size=0.75) +
@@ -201,8 +179,6 @@ plot_ab
 save_plot("Fig_1.png", fig = plot_ab, width=17, height=13)
 save_plot("Fig_1.svg", fig = plot_ab, width=17, height=13)
 
-
-
 #### Descriptives
 
 data <- read.csv("D:/Anna/Dropbox/Projects/2024_San Diego/School reforms/Data/data_for_analysis_spei05.csv")[-1]
@@ -220,7 +196,6 @@ data <- data %>%
   drop_na() %>% 
   mutate(drought_primReformExpYes = ifelse(drought==1 & primReformExp==1, 1, 0)) %>% 
   mutate(drought_primReformExpNo = ifelse(drought==1 & primReformExp==0, 1, 0))
-
 
 desc_age_0_5 <- data %>% 
   group_by(CountryName, primReformExp) %>% 
